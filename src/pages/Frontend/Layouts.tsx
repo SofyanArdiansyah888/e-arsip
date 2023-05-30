1a0dd0817e59f4d8fa7c340a4017868c962bbebc
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-05-31 06:22:41 +0800

    change wa position

--- src/pages/Frontend/Layouts.tsx
+++ src/pages/Frontend/Layouts.tsx
@@ -7,12 +7,13 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     <div>
       
       {children}
-      <Footer />
       <a href="https://wa.me/08114344494?text=Hallo%20sekda" target="_blank">
             <div className="w-16 h-16 bg-green-700 sticky bottom-6 rounded-full left-[94%] flex items-center animate-bounce">
               <img src={waUrl} />
             </div>
           </a>
+      <Footer />
+    
     </div>
   );
 }
