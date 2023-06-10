74fb96caab7b35861f41f7f325e3bb77b27a5691
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-06-10 17:10:09 +0800

    change whatsapp message

--- src/pages/Frontend/Layouts.tsx
+++ src/pages/Frontend/Layouts.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     <div>
       
       {children}
-      <a href="https://wa.me/08114344494?text=Hallo%20sekda" target="_blank" className="fixed right-4 bottom-4">
+      <a href="https://wa.me/08114344494?text=Hallo%Admin" target="_blank" className="fixed right-4 bottom-4">
             <div className="w-16 h-16 bg-green-700 sticky bottom-6 rounded-full left-[94%] flex items-center animate-bounce">
               <img src={waUrl} />
             </div>
