3bdf5df5ba53ee4b4dcb32bb13a448fe8d4f716f
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-06-12 12:03:30 +0800

    add email sending proces

--- src/pages/Frontend/Pengaduan.tsx
+++ src/pages/Frontend/Pengaduan.tsx
@@ -1,15 +1,58 @@
-import { useGet } from "@/hooks/useApi";
-import sekdaUrl from "@/assets/images/sekda.jpg";
-import LokasiSekda from "@/models/LokasiSekda.entity";
-import { GetDetailPayload } from "@/models/GenericPayload";
-import Layout from "./Layouts";
-import Navbar from "./Components/Navbar";
 import { useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import Layout from "./Layouts";
+import { usePost } from "@/hooks/useApi";
+import { useAppDispatch } from "@/stores/hooks";
+import { setNotification } from "@/stores/apps/notificationSlice";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup
+  .object({
+    nama: yup.string().required(),
+    email: yup.string().required(),
+    subjek: yup.string().required(),
+    pengaduan: yup.string().required(),
+  })
+  .required();
+type FormData = yup.InferType<typeof schema>;
+
 export default function Pengaduan() {
-  const { data, isLoading } = useGet<GetDetailPayload<LokasiSekda>>({
-    name: "lokasi",
-    endpoint: "lokasi",
+  const dispatch = useAppDispatch();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    control,
+    reset,
+    watch,
+    setValue,
+    setError,
+  } = useForm<FormData>({
+    mode: "onChange",
+    resolver: yupResolver(schema),
   });
+  const { mutate, isLoading } = usePost<FormData>({
+    endpoint: "pengaduan",
+    name: "pengaduan",
+    onSuccessCallback: () => {
+      reset()
+      alert('Berhasil Mengirim Pengaduan')
+          // dispatch(setNotification({
+          //   message: 'Berhasil Mengirim Pengaduan',
+          //   status: 'success'
+          // }))
+    },
+    onErrorCallback: (error: any) => {
+      alert('Gagal Mengirim Pengaduan')
+      // dispatch(setNotification({
+      //   message: 'Gagal Mengirim Pengaduan',
+      //   status: 'error'
+      // }))
+    },
+  });
+
   useEffect(() => {
     const body = document.getElementsByTagName("body")[0];
     body.style.padding = "0";
@@ -29,6 +72,9 @@ export default function Pengaduan() {
         <h1 className="text-3xl font-semibold ">Pengaduan</h1>
         <div className="border-b-4 border-gray-200 w-[350px] mb-20 mt-2 mx-auto"></div>
 
+<form onSubmit={handleSubmit((data) => {
+mutate({...data})
+})}>
         <div className="max-w-xl mx-auto px-4 py-6 bg-white rounded-md shadow-md space-y-4 text-left">
           <div>
             <h2 className="text-lg font-semibold">LAYANAN PENGADUAN</h2>
@@ -40,14 +86,15 @@ export default function Pengaduan() {
 
           <div className="text-left">
             <label
-              htmlFor="first_name"
+              htmlFor="nama"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Nama Anda
             </label>
             <input
               type="text"
-              id="first_name"
+              id="nama"
+              {...register('nama')}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
             />
@@ -55,49 +102,53 @@ export default function Pengaduan() {
 
           <div className="text-left">
             <label
-              htmlFor="first_name"
+              htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Email / No Hp
             </label>
             <input
               type="text"
-              id="first_name"
+              id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
+              {...register('email')}
             />
           </div>
 
           <div className="text-left">
             <label
-              htmlFor="first_name"
+              htmlFor="subjek"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Subjek
             </label>
             <input
               type="text"
-              id="first_name"
+              id="subjek"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
+              {...register('subjek')}
             />
           </div>
 
           <div className="text-left">
             <label
-              htmlFor="first_name"
+              htmlFor="pengaduan"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Pengaduan Anda
             </label>
             <textarea
-              id="first_name"
+              id="pengaduan"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               required
+              {...register('pengaduan')}
             />
           </div>
-          <button className="bg-green-600 text-white font-semibold px-2 py-2 block w-full rounded-md">Kirim Pesan</button>
+          <button className="bg-green-600 text-white font-semibold px-2 py-2 block w-full rounded-md">{isLoading ? "Loading..." : "Kirim Pesan"}</button>
         </div>
+        </form>
       </section>
     </Layout>
   );
