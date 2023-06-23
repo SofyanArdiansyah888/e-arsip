b400010fee99e29b3167afa425bfb486fdaf2e5c
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-06-23 09:34:53 +0800

    add status posisi sekda

--- src/pages/Dashboard/Surat/surat.tsx
+++ src/pages/Dashboard/Surat/surat.tsx
@@ -1,18 +1,16 @@
 //@ts-nocheck
 import Button from "@/base-components/Button";
-import { Menu } from "@/base-components/Headless";
+import {Menu} from "@/base-components/Headless";
 import Lucide from "@/base-components/Lucide";
-import {
-  AddButton
-} from "@/components/Buttons/Buttons";
-import { DeleteModal } from "@/components/Modals/Modals";
+import {AddButton} from "@/components/Buttons/Buttons";
+import {DeleteModal} from "@/components/Modals/Modals";
 import SkeletonTable from "@/components/Skeletons/SkeletonTable";
-import { useDelete, useGet, usePut } from "@/hooks/useApi";
-import { GetPayload } from "@/models/GenericPayload";
+import {useDelete, useGet, usePut} from "@/hooks/useApi";
+import {GetPayload} from "@/models/GenericPayload";
 import SuratEntity from "@/models/Surat.entity";
-import { setNotification } from "@/stores/apps/notificationSlice";
-import { useAppDispatch } from "@/stores/hooks";
-import { useEffect, useState } from "react";
+import {setNotification} from "@/stores/apps/notificationSlice";
+import {useAppDispatch} from "@/stores/hooks";
+import {useEffect, useState} from "react";
 import ReactPaginate from "react-paginate";
 import ExportButton from "./components/ExportButton";
 import SuratTable from "./components/SuratTable";
@@ -21,198 +19,207 @@ import EditSurat from "./editSurat";
 import ImportModal from "./components/ImportModal";
 
 export default function Surat() {
-  const [createModal, setCreateModal] = useState(false);
-  const [isImportModal, setIsImportModal] = useState(false);
-  const [deleteModal, setDeleteModal] = useState(false);
-  const [editModal, setEditModal] = useState(false);
-  const [meta, setMeta] = useState<any>();
-  const [selectedItem, setSelectedItem] = useState<SuratEntity>();
-  const [page, setPage] = useState<number>(0);
-  const [limit, setLimit] = useState(10);
-  const [search, setSearch] = useState<string>("");
-  
-  const {
-    data: payload,
-    refetch,
-    isLoading,
-  } = useGet<GetPayload<SuratEntity>>({
-    name: "surats",
-    endpoint: `surats`,
-    page: page + 1,
-    limit,
-    search,
-  });
+    const [createModal, setCreateModal] = useState(false);
+    const [isImportModal, setIsImportModal] = useState(false);
+    const [deleteModal, setDeleteModal] = useState(false);
+    const [editModal, setEditModal] = useState(false);
+    const [meta, setMeta] = useState<any>();
+    const [selectedItem, setSelectedItem] = useState<SuratEntity>();
+    const [page, setPage] = useState<number>(0);
+    const [limit, setLimit] = useState(10);
+    const [search, setSearch] = useState<string>("");
 
-  const dispatch = useAppDispatch();
+    const {
+        data: payload,
+        refetch,
+        isLoading,
+    } = useGet<GetPayload<SuratEntity>>({
+        name: "surats",
+        endpoint: `surats`,
+        page: page + 1,
+        limit,
+        search,
+    });
 
-  const { mutate: updateStatus, isLoading: isUpdateLoading } = usePut({
-    name: "lokasi",
-    endpoint: "lokasi/1",
-    onSuccessCallback: () => {
-      dispatch(
-        setNotification({
-          message: "Berhasil Mengupdate Lokasi Sekda",
-          status: "success",
-        })
-      );
-    },
-  });
+    const dispatch = useAppDispatch();
 
-  const { mutate } = useDelete({
-    name: "surats",
-    endpoint: `surats/${selectedItem?.id}`,
-    onSuccessCallback: () => {
-      dispatch(
-        setNotification({
-          message: "Berhasil Menghapus Surat",
-          status: "success",
-        })
-      );
-      setDeleteModal(false);
-    },
-  });
+    const {mutate: updateStatus, isLoading: isUpdateLoading} = usePut({
+        name: "lokasi",
+        endpoint: "lokasi/1",
+        onSuccessCallback: () => {
+            dispatch(
+                setNotification({
+                    message: "Berhasil Mengupdate Lokasi Sekda",
+                    status: "success",
+                })
+            );
+        },
+    });
 
-  useEffect(() => {
-    if (payload) {
-      setMeta(payload.meta);
-    }
-  }, [payload]);
+    const {mutate} = useDelete({
+        name: "surats",
+        endpoint: `surats/${selectedItem?.id}`,
+        onSuccessCallback: () => {
+            dispatch(
+                setNotification({
+                    message: "Berhasil Menghapus Surat",
+                    status: "success",
+                })
+            );
+            setDeleteModal(false);
+        },
+    });
 
-  useEffect(() => {
-    const delayDebounceFn = setTimeout(() => {
-      setPage(0);
-      setLimit(10);
-      refetch();
-    }, 300);
-    return () => clearTimeout(delayDebounceFn);
-  }, [search]);
+    useEffect(() => {
+        if (payload) {
+            setMeta(payload.meta);
+        }
+    }, [payload]);
 
-  const handleUpdate = (
-    lokasi: "berada di kantor" | "tugas luar" | "tugas dalam daerah"
-  ) => {
-    updateStatus({ lokasi });
-  };
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            setPage(0);
+            setLimit(10);
+            refetch();
+        }, 300);
+        return () => clearTimeout(delayDebounceFn);
+    }, [search]);
 
-  return (
-    <>
-      <div className="flex flex-nowrap justify-between mt-10">
-        <h2 className=" text-lg font-medium intro-y">List Surat</h2>
-        <div className=" flex gap-2 ml-auto">
-          <AddButton title="Tambah" onClick={() => setCreateModal(true)} />
-          
-          <ExportButton />
+    const handleUpdate = (
+        lokasi: "berada di kantor" | "tugas luar" | "tugas dalam daerah" | "libur" | "diluar jam kerja" | '-'
+    ) => {
+        updateStatus({lokasi});
+    };
 
-          <Button variant="instagram" size="sm" onClick={() => setIsImportModal(true)} >
-            <Lucide icon="Table" className="mr-2" />
-            Import Excel
-          </Button>
-          <Menu className="z-50">
-            <Menu.Button
-              variant="primary"
-              className="bg-primary text-white flex px-3 py-3 rounded-md text-xs"
-            >
-              {isUpdateLoading ? "Loading..." : "Update Status Sekda"}
-              <Lucide icon="ChevronDown" className="w-4 h-4 ml-2" />
-            </Menu.Button>
-            <Menu.Items className="w-48">
-              <Menu.Item onClick={() => handleUpdate("berada di kantor")}>
-                Berada Di Kantor
-              </Menu.Item>
-              <Menu.Item onClick={() => handleUpdate("tugas luar")}>
-                Tugas Luar
-              </Menu.Item>
-              <Menu.Item onClick={() => handleUpdate("tugas dalam daerah")}>
-                Tugas Dalam Daerah
-              </Menu.Item>
-            </Menu.Items>
-          </Menu>
-        </div>
-      </div>
+    return (
+        <>
+            <div className="flex flex-nowrap justify-between mt-10">
+                <h2 className=" text-lg font-medium intro-y">List Surat</h2>
+                <div className=" flex gap-2 ml-auto">
+                    <AddButton title="Tambah" onClick={() => setCreateModal(true)}/>
 
-      <div className="grid grid-cols-12 gap-6 mt-5">
-        <div className="flex flex-wrap justify-end  col-span-12 mt-2 sm:flex-nowrap">
-          <div className="relative w-full mt-3 ml-auto sm:w-auto sm:mt-0">
-            <Lucide
-              icon="Search"
-              className="absolute inset-y-0 left-0 z-10 w-4 h-4 my-auto ml-3 text-slate-500"
+                    <ExportButton/>
+
+                    <Button variant="instagram" size="sm" onClick={() => setIsImportModal(true)}>
+                        <Lucide icon="Table" className="mr-2"/>
+                        Import Excel
+                    </Button>
+                    <Menu className="z-50">
+                        <Menu.Button
+                            variant="primary"
+                            className="bg-primary text-white flex px-3 py-3 rounded-md text-xs"
+                        >
+                            {isUpdateLoading ? "Loading..." : "Update Status Sekda"}
+                            <Lucide icon="ChevronDown" className="w-4 h-4 ml-2"/>
+                        </Menu.Button>
+                        <Menu.Items className="w-48">
+                            <Menu.Item onClick={() => handleUpdate("berada di kantor")}>
+                                Berada Di Kantor
+                            </Menu.Item>
+                            <Menu.Item onClick={() => handleUpdate("tugas luar")}>
+                                Tugas Luar
+                            </Menu.Item>
+                            <Menu.Item onClick={() => handleUpdate("tugas dalam daerah")}>
+                                Tugas Dalam Daerah
+                            </Menu.Item>
+                            <Menu.Item onClick={() => handleUpdate("libur")}>
+                                Libur
+                            </Menu.Item>
+                            <Menu.Item onClick={() => handleUpdate("diluar jam kerja")}>
+                                Diluar Jam Kerja
+                            </Menu.Item>
+                            <Menu.Item onClick={() => handleUpdate("-")}>
+                                -
+                            </Menu.Item>
+                        </Menu.Items>
+                    </Menu>
+                </div>
+            </div>
+
+            <div className="grid grid-cols-12 gap-6 mt-5">
+                <div className="flex flex-wrap justify-end  col-span-12 mt-2 sm:flex-nowrap">
+                    <div className="relative w-full mt-3 ml-auto sm:w-auto sm:mt-0">
+                        <Lucide
+                            icon="Search"
+                            className="absolute inset-y-0 left-0 z-10 w-4 h-4 my-auto ml-3 text-slate-500"
+                        />
+                        <input
+                            type="text"
+                            className="w-full px-10 sm:w-64 !box"
+                            placeholder="Cari..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+
+                        <Lucide
+                            icon="X"
+                            className="absolute inset-y-0 right-2 z-10 w-4 h-4 my-auto ml-3 text-slate-500"
+                            onClick={() => setSearch("")}
+                        />
+                    </div>
+                </div>
+
+                {!isLoading ? (
+                    <SuratTable
+                        handleEdit={(Surat: SuratEntity) => {
+                            setEditModal(true);
+                            setSelectedItem(Surat);
+                        }}
+                        handleDelete={(Surat: SuratEntity) => {
+                            setDeleteModal(true);
+                            setSelectedItem(Surat);
+                        }}
+                        payload={payload}
+                    />
+                ) : (
+                    <SkeletonTable/>
+                )}
+
+                <div className="col-span-12 flex justify-center">
+                    <ReactPaginate
+                        nextLabel="next"
+                        onPageChange={(data) => setPage(data.selected)}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={2}
+                        initialPage={page}
+                        pageCount={meta?.last_page}
+                        previousLabel="prev"
+                        pageClassName="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
+                        pageLinkClassName="page-link"
+                        previousClassName="page-item"
+                        previousLinkClassName="capitalize px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 "
+                        nextClassName="capitalize px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 "
+                        breakLabel="..."
+                        breakClassName="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
+                        containerClassName="flex flex-wrap items-center intro-y sm:flex-row sm:flex-nowrap "
+                        activeClassName="px-3 py-2 text-slate-100 border border-gray-300 bg-slate-700 hover:bg-slate-600 "
+                        renderOnZeroPageCount={undefined}
+                    />
+                </div>
+            </div>
+            <DeleteModal
+                isModal={deleteModal}
+                handleConfirm={() => mutate({})}
+                handleCancel={() => setDeleteModal(false)}
+                title={selectedItem?.nomor_surat}
             />
-            <input
-              type="text"
-              className="w-full px-10 sm:w-64 !box"
-              placeholder="Cari..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
+
+            <CreateSurat
+                isModal={createModal}
+                handleCancel={() => setCreateModal(false)}
             />
 
-            <Lucide
-              icon="X"
-              className="absolute inset-y-0 right-2 z-10 w-4 h-4 my-auto ml-3 text-slate-500"
-              onClick={() => setSearch("")}
+            <EditSurat
+                isModal={editModal}
+                selectedItem={selectedItem}
+                handleCancel={() => setEditModal(false)}
             />
-          </div>
-        </div>
-
-        {!isLoading ? (
-          <SuratTable
-            handleEdit={(Surat: SuratEntity) => {
-              setEditModal(true);
-              setSelectedItem(Surat);
-            }}
-            handleDelete={(Surat: SuratEntity) => {
-              setDeleteModal(true);
-              setSelectedItem(Surat);
-            }}
-            payload={payload}
-          />
-        ) : (
-          <SkeletonTable />
-        )}
-
-        <div className="col-span-12 flex justify-center">
-          <ReactPaginate
-            nextLabel="next"
-            onPageChange={(data) => setPage(data.selected)}
-            pageRangeDisplayed={3}
-            marginPagesDisplayed={2}
-            initialPage={page}
-            pageCount={meta?.last_page}
-            previousLabel="prev"
-            pageClassName="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-            pageLinkClassName="page-link"
-            previousClassName="page-item"
-            previousLinkClassName="capitalize px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 "
-            nextClassName="capitalize px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 "
-            breakLabel="..."
-            breakClassName="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
-            containerClassName="flex flex-wrap items-center intro-y sm:flex-row sm:flex-nowrap "
-            activeClassName="px-3 py-2 text-slate-100 border border-gray-300 bg-slate-700 hover:bg-slate-600 "
-            renderOnZeroPageCount={undefined}
-          />
-        </div>
-      </div>
-      <DeleteModal
-        isModal={deleteModal}
-        handleConfirm={() => mutate({})}
-        handleCancel={() => setDeleteModal(false)}
-        title={selectedItem?.nomor_surat}
-      />
-
-      <CreateSurat
-        isModal={createModal}
-        handleCancel={() => setCreateModal(false)}
-      />
-
-      <EditSurat
-        isModal={editModal}
-        selectedItem={selectedItem}
-        handleCancel={() => setEditModal(false)}
-      />
-      <ImportModal
-      isModal={isImportModal}
-      handleCancel={() => setIsImportModal(false)}
-      />
-    </>
-  );
+            <ImportModal
+                isModal={isImportModal}
+                handleCancel={() => setIsImportModal(false)}
+            />
+        </>
+    );
 }
 
 
