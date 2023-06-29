ba3deb374bc10a613f7f40fb1843f73a049f2874
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-06-29 20:20:15 +0800

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,32 @@
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import "./assets/css/app.css";
 import ScrollToTop from "./base-components/ScrollToTop";
-import { AuthProvider } from "./providers/AuthProvider";
+import {AuthProvider} from "./providers/AuthProvider";
 
 import Router from "./router";
-import { store } from "./stores/store";
+import {store} from "./stores/store";
+import {Analytics} from '@vercel/analytics/react';
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
     },
-  },
 });
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <Router />
-        </Provider>
-      </QueryClientProvider>
-    </AuthProvider>
-    <ScrollToTop />
-  </BrowserRouter>
+    <BrowserRouter>
+        <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <Provider store={store}>
+                    <Router/>
+                    <Analytics/>
+                </Provider>
+            </QueryClientProvider>
+        </AuthProvider>
+        <ScrollToTop/>
+    </BrowserRouter>
 );
