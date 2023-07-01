c9cbedf506b148d52ff45fc955e52d0886385878
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-07-01 14:42:11 +0800

    change posisi sekda timestamp

--- src/models/LokasiSekda.entity.ts
+++ src/models/LokasiSekda.entity.ts
@@ -1,4 +1,5 @@
 export default interface LokasiSekda {
     lokasi: "berada di kantor" | "tugas luar" | 'tugas dalam daerah'
     updated_at: string;
+    tempe?: string;
 }
--- src/pages/Frontend/PosisiSekda.tsx
+++ src/pages/Frontend/PosisiSekda.tsx
@@ -4,13 +4,12 @@ import LokasiSekda from "@/models/LokasiSekda.entity";
 import {GetDetailPayload} from "@/models/GenericPayload";
 import Layout from "./Layouts";
 import Navbar from "./Components/Navbar";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import dayjs from "dayjs";
 
 export default function PosisiSekda() {
     const {data, isLoading} = useGet<GetDetailPayload<LokasiSekda>>({
-        name: "lokasi",
-        endpoint: "lokasi",
+        name: "lokasi", endpoint: "lokasi",
     });
     useEffect(() => {
         const body = document.getElementsByTagName("body")[0];
@@ -21,57 +20,80 @@ export default function PosisiSekda() {
             body.style.padding = "16px";
         };
     }, []);
-    return (
-        <Layout>
-            <Navbar/>
-            <section
-                id="sambutan"
-                className="pb-48 pt-40 text-center bg-zinc-50   min-h-[calc(100vh-70px)]  lg:px-48"
-            >
-                <div className="flex flex-col xl:flex-row  items-center">
-                    <div className=" h-auto mb-24 xl:mb-0 relative">
-                        <img
-                            src={sekdaUrl}
-                            className=" rounded-lg h-80  w-72 mx-auto  bg-white object-fill relative z-10"
-                        />
+    return (<Layout>
+        <Navbar/>
+        <section
+            id="sambutan"
+            className="pb-48 pt-40 text-center bg-zinc-50   min-h-[calc(100vh-70px)]  lg:px-48"
+        >
+            <div className="flex flex-col xl:flex-row  items-center">
+                <div className=" h-auto mb-24 xl:mb-0 relative">
+                    <img
+                        src={sekdaUrl}
+                        className=" rounded-lg h-80  w-72 mx-auto  bg-white object-fill relative z-10"
+                    />
 
-                        {!isLoading && (
-                            <>
-                                <div
-                                    className={`mt-8 p-3  rounded-xl font-semibold  text-white capitalize ${
-                                        data?.data.lokasi === "berada di kantor"
-                                            ? "bg-green-600"
-                                            : "bg-red-600"
-                                    } z-10 relative`}
-                                >
-                                    {data?.data.lokasi} <br/>
-                                </div>
-                                <div className={"mt-4 font-semibold"}>Update
-                                    : {dayjs(data?.data.updated_at).format('D MMMM YYYY, hh:mm')} WIT
-                                </div>
-                            </>
-                        )}
-
-                        <div className="bg-gray-200 h-72 w-64 rounded-md absolute bottom-16 -left-4 opacity-50  "></div>
-                    </div>
-
-                    <div className="w-3/4 text-left space-y-8 mx-8 lg:ml-24 self-center mb-16 ">
-                        <h3 className="text-4xl font-semibold capitalize">
-                            Posisi Sekda
-                        </h3>
-                        <div className={"space-y-3"}>
-                            <ItemSekda title={"Nama"} description={"Drs. Samsuddin Abdul Kadir, M.Si"}/>
-                            <ItemSekda title={"Nip"} description={"19701012 199101 1 003"}/>
-                            <ItemSekda title={"Pangkat/Gol"} description={" Pembina Utama, IV/e"}/>
-                            <ItemSekda title={"Tempat/Tanggal Lahir"} description={"Ternate, 12 Oktober 1970."}/>
-
+                    {!isLoading && (<>
+                        <ButtonStatus lokasi={data?.data.lokasi}/>
+                        <div className={"mt-4 font-semibold"}>Update
+                            : {dayjs(data?.data.tempe).format('D MMMM YYYY, hh:mm')} WIT
                         </div>
+                    </>)}
+
+                    <div className="bg-gray-200 h-72 w-64 rounded-md absolute bottom-16 -left-4 opacity-50  "></div>
+                </div>
+
+                <div className="w-3/4 text-left space-y-8 mx-8 lg:ml-24 self-center mb-16 ">
+                    <h3 className="text-4xl font-semibold capitalize">
+                        Posisi Sekda
+                    </h3>
+                    <div className={"space-y-3"}>
+                        <ItemSekda title={"Nama"} description={"Drs. Samsuddin Abdul Kadir, M.Si"}/>
+                        <ItemSekda title={"Nip"} description={"19701012 199101 1 003"}/>
+                        <ItemSekda title={"Pangkat/Gol"} description={" Pembina Utama, IV/e"}/>
+                        <ItemSekda title={"Tempat/Tanggal Lahir"} description={"Ternate, 12 Oktober 1970."}/>
 
                     </div>
+
                 </div>
-            </section>
-        </Layout>
-    );
+            </div>
+        </section>
+    </Layout>);
+}
+
+
+const ButtonStatus = ({lokasi}: { lokasi?: string }) => {
+    const [color, setColor] = useState("");
+    useEffect(() => {
+        switch (lokasi) {
+            case "berada di kantor":
+                setColor("bg-green-600");
+                break;
+            case "tugas luar":
+                setColor("bg-yellow-600");
+                break;
+            case "tugas dalam daerah":
+                setColor("bg-purple-600");
+                break;
+            case "libur":
+                setColor("bg-amber-600");
+                break;
+            case "diluar jam kerja":
+                setColor("bg-slate-600");
+                break;
+            default:
+                setColor("bg-red-600");
+                break;
+
+        }
+
+    }, [lokasi])
+
+    return <div
+        className={`mt-8 p-3   rounded-xl font-semibold  text-white capitalize ${color} z-10 relative`}
+    >
+        {lokasi} <br/>
+    </div>
 }
 
 export const ItemSekda = ({title, description}: { title: string, description: string }) => <div
